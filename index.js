120f6690f67b2d6057f1f87048e5f945105aac7e
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 21:00:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function setThisWithCall(fn, thisValue, arg) {
 function setThisWithApply(fn, thisValue, args) {
   return fn.apply(thisValue, args)
 }
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue)
+}
